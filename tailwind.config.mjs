48f77eac5ea71c981208949a486203050e3cc7b9
IkerMarcor <99061325+IkerMarcor@users.noreply.github.com>
2024-01-19 12:30:26 -0700

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+			colors: {
+				'dark-blue': '#13151a',
+				'blue': '#0D1C3A',
+				'gray': '#94a3b8',
+				'light-gray': '#e5e7eb',
+				'cyan':'#40a6ef',
+				'dark-cyan':'#2d8fd5',
+				'white':'#ffffff',
+				'green': '#16C55E',
+			}
+	},
+	plugins: [],
+}
